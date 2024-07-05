bed70f2eacabfd9bf432b209d0527392c796c085
somilkwatra <somilkwatra21@gmail.com>
2024-07-05 11:24:09 +0530

    hi

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const bodyParser=require('body-parser')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
 // Import routes
-const userRoutes = require('./routes/user');
+const userRoutes = require("./routes/user");
 const quizRouter = require("./routes/quiz");
 const gameRouter = require("./routes/game");
 dotenv.config();
@@ -14,30 +14,24 @@ app.use(express.json());
 
 app.use(cors());
 
-
 const PORT = process.env.PORT || 5000;
- 
-app.listen(PORT, () => { 
-    console.log(`Server Running on Port ${PORT}`);
+
+app.listen(PORT, () => {
+  console.log(`Server Running on Port ${PORT}`);
 });
 
-const connectDB = async () => {  
-    try {
-        await mongoose.connect(process.env.MONGO_URL, {
-        });
-        console.log('Successfully connected to the database');
-    } catch (err) {
-        console.error('Error connecting to the database:', err);
-        process.exit(1);
-    }
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {});
+    console.log("Successfully connected to the database");
+  } catch (err) {
+    console.error("Error connecting to the database:", err);
+    process.exit(1);
+  }
 };
 
 connectDB();
 
-
-app.use('/api/users', userRoutes);
-app.use('/api/quizes', quizRouter);
-app.use('/api/game',gameRouter);
-
-
-
+app.use("/api/users", userRoutes);
+app.use("/api/quizes", quizRouter);
+app.use("/api/game", gameRouter);
